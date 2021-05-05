cbbe105d8e6a70ae4d48120945bf2b584b8fdcc0
GingGingI <gingjingi@gmail.com>
2021-05-05 16:54:31 +0900

    방참여 및 데이터 송수신 구현중

--- js/main.js
+++ js/main.js
@@ -5,6 +5,20 @@ const mediaStreamConstraints = {
     'audio': false
 };
 
+const configuration = {
+    "iceServers": [
+        { 
+            "url": "stun:stun.zifori.me" },
+        {
+            "url": "turn:turn.zifori.me",
+            username: "turn",
+            credential: "turnpw",
+        }
+    ]
+}
+
+var isStarted = false
+
 let localVideo = document.querySelector('video#localVideo')
 let remoteVideo = document.querySelector('video#remoteVideo')
 
@@ -49,13 +63,40 @@ function connecToSocket(channel) {
     const url = "ws://www.zifori.me:7080/signaling/".concat(channel).concat('/').concat(userId)
     socket = new WebSocket(url);
 
-
-
+    socket.onmessage = onSocketMessage
 }
 
+function onSocketMessage(event) {
+    const msg = JSON.parse(event.data)
 
+    switch(msg.type) {
+        case "created":
+            console.log("server created")
+        break;
+        case "join" :
+            console.log('Another peer a requst to join room ' + msg.room);
 
-
+            maybeStart()
+            isChannelReady = true;
+        break;
+        case "got user media" :
+            console.log("joined : " + msg.room)
+            
+            isChannelReady = true
+        break;
+        case "candidate":
+            if (isStarted && isChannelReady) {
+                console.log('candidate received')
+                // ice candidate received
+                let candidate = new RTCIceCandidate({
+                    sdpMLineIndex: message.label,
+                    candidate: message.candidate
+                });
+                pc.addIceCandidate(candidate)
+            }
+        break;
+    }
+}
 
 ///////////////////////////////////////////////
 
@@ -74,6 +115,14 @@ function getLocalMediaStream(mediaStream) {
     trace("Received local stream.")
 }
 
+function maybeStart() {
+    if(!isStarted && typeof localStream !== 'undefined' && isChannelReady) {
+        createPeerConnection()
+        pc.addStream(localStream)
+        isStarted = true
+    }
+}
+
 function handleLocalMediaStreamError(error) {
     trace(`navigator.getUserMedia error: ${error.toString()}.`);
 }
@@ -82,8 +131,12 @@ function createPeerConnection() {
     try {
         pc = new RTCPeerConnection(null);
         pc.onicecandidate = handleIceCandidate
-    } catch {
-
+        // pc.onaddstream = handleRemoteStreamAdded //onRemoteStream Added
+        // pc.onremovestream = handleRemoteStreamRemoved //onRemoteStream removed
+    } catch(e) {
+        console.log('Failed to create PeerConnection : ' + e.message)
+        console.log('Can\'t create RTCPeerConnection object')
+        return;
     }
 }
 
@@ -107,5 +160,9 @@ function trace(text) {
   
     console.log(now, text);
 }
+
+window.onbeforeunload = function() {
+    sendMessage('bye');
+  };
 
 init()
